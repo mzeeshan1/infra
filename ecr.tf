4f41e4dfad2051764d63179a24266f23206fbc4b
Muhammad Zeeshan <muhammad.zeeshan@babelforce.com>
2026-05-06 21:04:53 +0200

    fix(ci): fix issues

--- ecr.tf
+++ ecr.tf
@@ -1,22 +1,21 @@
-# module "ecr" {
-#   git::https://github.com/mzeeshan1/infra-modules.git//aws/ecr?ref=aws-ecr-v1.2.0
-
-#   repositories = {
-#     subscription-reminder = {
-#       name         = "subscription-reminder"
-#       scan_on_push = true
-#       image_tag_mutability_exclusion_filters = [{
-#         filter      = "v*"
-#         filter_type = "WILDCARD"
-#       }]
-#     }
-#     subman = {
-#       name         = "subman"
-#       scan_on_push = true
-#       image_tag_mutability_exclusion_filters = [{
-#         filter      = "v*"
-#         filter_type = "WILDCARD"
-#       }]
-#     }
-#   }
-# }
+module "ecr" {
+  source = "git::https://github.com/mzeeshan1/infra-modules.git//aws/ecr?ref=aws-ecr-v1.2.0"
+  repositories = {
+    subscription-reminder = {
+      name         = "subscription-reminder"
+      scan_on_push = true
+      image_tag_mutability_exclusion_filters = [{
+        filter      = "v*"
+        filter_type = "WILDCARD"
+      }]
+    }
+    subman = {
+      name         = "subman"
+      scan_on_push = true
+      image_tag_mutability_exclusion_filters = [{
+        filter      = "v*"
+        filter_type = "WILDCARD"
+      }]
+    }
+  }
+}
