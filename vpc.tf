1a90f42a15897b2b32aaf89a07f2683fa8ab3dd2
Muhammad Zeeshan <muhammad.zeeshan@babelforce.com>
2026-05-06 21:28:27 +0200

    feat(ci): trigger ci

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,11 @@
-module "vpc" {
-  source = "git::https://github.com/mzeeshan1/infra-modules.git//aws/vpc?ref=aws-vpc-v1.0.0"
-  vpc = {
-    hub = {
-      cidr                       = "10.100.0.0/16"
-      azs                        = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-      public_subnets             = ["10.100.0.0/19", "10.100.32.0/19", "10.100.64.0/19"]
-      manage_default_network_acl = false
-    }
-  }
-}
+# module "vpc" {
+#   source = "git::https://github.com/mzeeshan1/infra-modules.git//aws/vpc?ref=aws-vpc-v1.0.0"
+#   vpc = {
+#     hub = {
+#       cidr                       = "10.100.0.0/16"
+#       azs                        = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+#       public_subnets             = ["10.100.0.0/19", "10.100.32.0/19", "10.100.64.0/19"]
+#       manage_default_network_acl = false
+#     }
+#   }
+# }
