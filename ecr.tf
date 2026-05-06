c34399ff09e9bc2d5e5ee2c9701f93420420ce3c
Muhammad Zeeshan <muhammad.zeeshan@babelforce.com>
2026-05-06 15:36:12 +0200

    add files

--- ecr.tf
+++ ecr.tf
@@ -1,22 +1,22 @@
-module "ecr" {
-  source = "/Users/muhammadzeeshan/Work/MyWork/infra-modules/aws/ecr"
+# module "ecr" {
+#   source = "/Users/muhammadzeeshan/Work/MyWork/infra-modules/aws/ecr"
 
-  repositories = {
-    subscription-reminder = {
-      name         = "subscription-reminder"
-      scan_on_push = true
-      image_tag_mutability_exclusion_filters = [{
-        filter      = "v*"
-        filter_type = "WILDCARD"
-      }]
-    }
-    subman = {
-      name         = "subman"
-      scan_on_push = true
-      image_tag_mutability_exclusion_filters = [{
-        filter      = "v*"
-        filter_type = "WILDCARD"
-      }]
-    }
-  }
-}
+#   repositories = {
+#     subscription-reminder = {
+#       name         = "subscription-reminder"
+#       scan_on_push = true
+#       image_tag_mutability_exclusion_filters = [{
+#         filter      = "v*"
+#         filter_type = "WILDCARD"
+#       }]
+#     }
+#     subman = {
+#       name         = "subman"
+#       scan_on_push = true
+#       image_tag_mutability_exclusion_filters = [{
+#         filter      = "v*"
+#         filter_type = "WILDCARD"
+#       }]
+#     }
+#   }
+# }
