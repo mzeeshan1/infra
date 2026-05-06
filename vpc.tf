a79aaafb72b66ea8105a1d20d33f0b542376bfad
Muhammad Zeeshan <muhammad.zeeshan@babelforce.com>
2026-05-06 15:26:06 +0200

    feat: create hub vpc

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,11 @@
-# module "vpc" {
-#   source = "/Users/muhammadzeeshan/Work/MyWork/infra-modules/aws/vpc"
-#   vpc = {
-#     hub = {
-#       cidr                       = "10.100.0.0/16"
-#       azs                        = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-#       public_subnets             = ["10.100.0.0/19", "10.100.32.0/19", "10.100.64.0/19"]
-#       manage_default_network_acl = false
-#     }
-#   }
-# }
+module "vpc" {
+  source = "/Users/muhammadzeeshan/Work/MyWork/infra-modules/aws/vpc"
+  vpc = {
+    hub = {
+      cidr                       = "10.100.0.0/16"
+      azs                        = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+      public_subnets             = ["10.100.0.0/19", "10.100.32.0/19", "10.100.64.0/19"]
+      manage_default_network_acl = false
+    }
+  }
+}
